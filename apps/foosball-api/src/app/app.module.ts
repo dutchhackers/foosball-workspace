22fd7526152f7970840ffa0e033bcb31ce74defe
Michael Schilling <mschilling@move4mobile.com>
2022-09-25 22:20:09 +0200

    chore: add match data

--- apps/foosball-api/src/app/app.module.ts
+++ apps/foosball-api/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { DataModule } from '@foosball/data';
 import { CoreModule } from '@foosball/core';
+import { DataModule } from '@foosball/data';
+import { MatchesController } from './controllers/matches.controller';
 import { PlayersController } from './controllers/players.controller';
+import { MatchService } from './services/match.service';
 import { PlayerService } from './services/player.service';
 
 @Module({
   imports: [DataModule, CoreModule],
-  controllers: [AppController, PlayersController],
-  providers: [AppService, PlayerService],
+  controllers: [AppController, PlayersController, MatchesController],
+  providers: [AppService, PlayerService, MatchService],
 })
 export class AppModule {}
---
+++ apps/foosball-api/src/app/services/match.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { DataService } from '@foosball/data';
+
+@Injectable()
+export class MatchService {
+  constructor(private readonly data: DataService) {}
+
+  async test() {
+    return this.getData();
+  }
+
+  private async getData() {
+    const snapshotItems = await this.data.collection('matches').orderBy('matchDate', 'desc').limitToLast(5).get();
+    if (!snapshotItems.size) {
+      return [];
+    }
+    return snapshotItems.docs.map(snapshot => this.map(snapshot.data()));
+  }
+
+  private map(data: FirebaseFirestore.DocumentData) {
+    return {
+      ...data,
+      id: 1,
+      // createdAt: FirestoreTimestampToDate(data.createdAt),
+      // startedAt: FirestoreTimestampToDate(data.startedAt),
+      // closedAt: FirestoreTimestampToDate(data.closedAt),
+    };
+  }
+}
