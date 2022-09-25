d9e5ccd6566c8bd55b7602b6592322afed507000
Michael Schilling <mschilling@move4mobile.com>
2022-09-25 22:14:15 +0200

    chore: bind players datta

--- apps/foosball-api/src/app/services/player.service.ts
+++ apps/foosball-api/src/app/services/player.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { DataService } from '@foosball/data';
 
 @Injectable()
 export class PlayerService {
-  test() {
-    return [1, 2, 3];
+  constructor(private readonly data: DataService) {}
+
+  async test() {
+    return this.getData();
+  }
+
+  private async getData() {
+    const snapshotItems = await this.data
+      .collection('players')
+      // .orderBy('createdAt', 'desc')
+      // .limitToLast(5)
+      .get();
+    if (!snapshotItems.size) {
+      return [];
+    }
+    return snapshotItems.docs.map(snapshot => this.map(snapshot.data()));
+  }
+
+  private map(data: FirebaseFirestore.DocumentData) {
+    return {
+      ...data,
+      id: 1,
+      // createdAt: FirestoreTimestampToDate(data.createdAt),
+      // startedAt: FirestoreTimestampToDate(data.startedAt),
+      // closedAt: FirestoreTimestampToDate(data.closedAt),
+    };
   }
 }
