1cc85e8ca8a983417e2ab26ab153ea5a8fd6933e
Michael Schilling <mschilling@move4mobile.com>
2022-10-02 14:51:12 +0200

    chore: fix tests

---
+++ apps/slackbot-api/jest.config.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+export default {
+  displayName: 'slackbot-api',
+
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/slackbot-api',
+  preset: '../../jest.preset.js',
+};
