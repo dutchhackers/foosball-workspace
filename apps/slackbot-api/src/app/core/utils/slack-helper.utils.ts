27a75688e51ae045215255faccfe5911b85274cd
Michael Schilling <mschilling@move4mobile.com>
2022-10-02 16:48:28 +0200

    chore: code improvement

--- apps/slackbot-api/src/app/core/utils/slack-helper.utils.ts
+++ apps/slackbot-api/src/app/core/utils/slack-helper.utils.ts
@@ -135,24 +135,3 @@ function applyFilter(items: Player[], filter: IDefaultLeaderboardOpts): Player[]
 
   return result;
 }
-
-// TODO: refactor into SlackApi -> Core
-export function addViewedBySnippetToBlock(blocks: any[], slackUserId: string): any[] {
-  if (!slackUserId) {
-    return blocks;
-  }
-  const response = [
-    ...blocks,
-    {
-      type: 'context',
-      elements: [
-        {
-          type: 'mrkdwn',
-          text: `Requested by <@${slackUserId}>`,
-        },
-      ],
-    },
-  ];
-  console.log('addViewedBySnippetToBlock', response);
-  return response;
-}
