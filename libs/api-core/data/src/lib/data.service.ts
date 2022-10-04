9df0c7e5acd59b4e6153d34da938ad7aa08faef6
Michael Schilling <mschilling@move4mobile.com>
2022-10-04 21:41:41 +0200

    chore: DataService cleanup

--- libs/api-core/data/src/lib/data.service.ts
+++ libs/api-core/data/src/lib/data.service.ts
@@ -1,22 +1,13 @@
-import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import * as firebase from 'firebase-admin';
 
 @Injectable()
-export class DataService extends firebase.firestore.Firestore implements OnModuleInit, OnModuleDestroy {
+export class DataService extends firebase.firestore.Firestore implements OnModuleInit {
   constructor() {
     super();
   }
 
-  public async onModuleDestroy() {
-    // await this.$disconnect();
-  }
-
   public async onModuleInit() {
-    // const app = firebase.initializeApp({
-    //   credential: firebase.credential.applicationDefault()
-    // }, 'firestoreApp');
-
-    // More specific init needed?
-    Logger.log('Firestore client connected'); // Example of logging
+    Logger.debug('Firestore client connected');
   }
 }
