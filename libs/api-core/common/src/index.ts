3bcbc9d054b61443f06727931334192ba1ae7233
Michael Schilling <mschilling@move4mobile.com>
2022-10-04 21:25:14 +0200

    cohre: restructure enums + cleanup

--- libs/api-core/common/src/index.ts
+++ libs/api-core/common/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/common.module';
+export * from './lib/enums';
 export * from './lib/models';
 export * from './lib/services';
 export * from './lib/utils';
--- libs/api-core/common/src/lib/enums/index.ts
+++ libs/api-core/common/src/lib/enums/index.ts
@@ -1,2 +1,4 @@
+// ENUMS aren't used yet
+export * from './match-event-type.enum';
 export * from './match-status.enum';
 export * from './team-side.enum';
