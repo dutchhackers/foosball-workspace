d48c4010a5d7bba4b0514c5c0f123e13c613812c
Michael Schilling <mschilling@move4mobile.com>
2022-10-04 22:52:23 +0200

    chore: add DataController

--- apps/foosball-api/src/app/app.module.ts
+++ apps/foosball-api/src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 
 import { AuthModule } from '@foosball/api/auth';
-import { MatchService, PlayerService, StatsService } from '@foosball/api/common';
+import { DataMartService, MatchService, PlayerService, StatsService } from '@foosball/api/common';
 import { CoreModule } from '@foosball/api/core';
 import { DataModule } from '@foosball/api/data';
 import { MatchesController } from './controllers/matches.controller';
 import { PlayersController } from './controllers/players.controller';
 import { AuthController } from './controllers/auth.controller';
+import { DataController } from './controllers/data.controller';
 
 @Module({
   imports: [DataModule, CoreModule, AuthModule],
-  controllers: [AuthController, PlayersController, MatchesController],
-  providers: [PlayerService, MatchService, StatsService],
+  controllers: [AuthController, PlayersController, MatchesController, DataController],
+  providers: [PlayerService, MatchService, StatsService, DataMartService],
 })
 export class AppModule {}
