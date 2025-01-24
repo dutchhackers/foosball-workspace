650d23eacbc9cb216c4faff086a2f007837c4185
Michael Schilling <michael.schilling@moveagency.com>
2025-01-24 21:36:10 +0100

    rename statistics module

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -3,5 +3,6 @@ export * from './enums';
 export * from './interfaces';
 export * from './models';
 export * from './services';
+export * from './statistics';
 export * from './types';
 export * from './utils';
